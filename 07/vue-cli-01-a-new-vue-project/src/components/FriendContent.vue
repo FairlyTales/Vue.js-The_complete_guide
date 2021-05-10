8f4ed5e2e2ee21f6c53cbc6867ec91cd691e478d
kirill_khnychkin <khnychkin_kirill@tutanota.com>
2021-05-10 19:19:44 +0300

    finished 07

---
+++ 07/vue-cli-01-a-new-vue-project/src/components/FriendContent.vue
@@ -0,0 +1,36 @@
+<template>
+  <li>
+    <h2>
+      {{ friend.name }}
+    </h2>
+    <button @click="toggleDetails">Show detail</button>
+    <button @click="toggleDetails">{{ detailsAreVisible ? 'Hide' : 'Show' }} Details</button>
+    <ul v-if="detailsAreVisible">
+      <li>
+        <b>Phone:</b>
+        {{ friend.phone}}
+      </li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      detailsAreVisible: false,
+      friend: {
+        id: 'id',
+        name: 'name',
+        phone: '1231 231 2312',
+        email: 'asda@sald.com'
+      }
+    }
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    }
+  }
+}
+</script>
---
+++ 07/vue-cli-01-a-new-vue-project/src/main.js
@@ -0,0 +1,9 @@
+import { createApp } from 'vue';
+import App from './App.vue';
+import FriendContent from "@/components/FriendContent";
+
+const app = createApp(App);
+
+app.component('friend-contact', FriendContent);
+
+app.mount('#app');
