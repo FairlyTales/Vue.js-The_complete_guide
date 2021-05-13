362381447fe313a4f76b73760e2b14d032faec31
kirill_khnychkin <khnychkin_kirill@tutanota.com>
2021-05-13 15:49:54 +0300

    finished 11

---
+++ 11/src/components/RatingControl.vue
@@ -0,0 +1,13 @@
+<template>
+  <ul>
+    <li>
+      <button>Poor</button>
+    </li>
+    <li>
+      <button>Average</button>
+    </li>
+    <li>
+      <button>Great</button>
+    </li>
+  </ul>
+</template>
